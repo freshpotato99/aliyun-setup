5de2e96fa072ae04da96ec6da06521dddb023a63
freshpotato99 <18679917958@163.com>
2025-08-08 13:47:43 +0800

    add tfstate

--- main.tf
+++ main.tf
@@ -1,16 +1,5 @@
-terraform {
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    alicloud = {
-      source  = "aliyun/alicloud"
-      version = "~> 1.209"  # 使用适合的版本
-    }
-  }
-}
-
 provider "alicloud" {
-  region = var.region
+  region = "cn-hangzhou"
 }
 
 # 这里可以添加你的数据源(data)查询
