a1f55be5f231562c61f49228ad0afa3531c0c973
freshpotato99 <18679917958@163.com>
2025-08-08 15:09:33 +0800

    add resource

--- main.tf
+++ main.tf
@@ -6,13 +6,3 @@ provider "alicloud" {
 resource "random_uuid" "default" {
 }
 
-# 创建存储空间
-resource "alicloud_oss_bucket" "bucket" {
-  bucket = substr("tf-example-${replace(random_uuid.default.result, "-", "")}", 0, 16)
-}
-
-# 设置存储空间的访问权限
-resource "alicloud_oss_bucket_acl" "bucket-ac"{
-  bucket = alicloud_oss_bucket.bucket.id
-  acl = "private"
-}
