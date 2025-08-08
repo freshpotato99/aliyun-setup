548d53c0d137b6e74d9a674428797b8c42d8b891
freshpotato99 <18679917958@163.com>
2025-08-08 14:23:21 +0800

    add tf apply

--- main.tf
+++ main.tf
@@ -7,8 +7,3 @@ resource "alicloud_oss_bucket" "simple_bucket" {
   bucket = "my-simple-tf-bucket"  # 改成全球唯一的名称
   acl    = "private"              # 基础访问权限
 }
-
-# 输出Bucket名称（可选）
-output "bucket_name" {
-  value = alicloud_oss_bucket.simple_bucket.bucket
-}
