c9c29c3a96b8c73a73a224b47048148ec864c303
freshpotato99 <18679917958@163.com>
2025-08-08 14:33:06 +0800

    add tf apply

--- main.tf
+++ main.tf
@@ -1,9 +1,36 @@
+# main.tf
 provider "alicloud" {
-  region = "cn-hangzhou"
+  region = "cn-hangzhou"  # 根据需求修改地域
 }
 
-# 创建最简单的OSS Bucket
-resource "alicloud_oss_bucket" "simple_bucket" {
-  bucket = "my-simple-tf-bucket"  # 改成全球唯一的名称
-  acl    = "private"              # 基础访问权限
+# RAM用户登录密码，
+variable "password" {
+  default = "Test@123456!"
+}
+
+# 保存AccessKey的文件名
+variable "accesskey_txt_name" {
+  default = "accesskey.txt"
+}
+
+resource "random_integer" "default" {
+  min = 10000
+  max = 99999
+}
+
+# RAM用户
+resource "alicloud_ram_user" "user" {
+  name = "tf_user_${random_integer.default.result}"
+}
+
+# RAM用户登录密码
+resource "alicloud_ram_login_profile" "profile" {
+  user_name = alicloud_ram_user.user.name
+  password  = var.password
+}
+
+# RAM用户AccessKey
+resource "alicloud_ram_access_key" "ak" {
+  user_name   = alicloud_ram_user.user.name
+  secret_file = var.accesskey_txt_name
 }
