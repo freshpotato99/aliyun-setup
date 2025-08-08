2e567f25d2b2dbe0fb7c2eea6dffaee01bb8ca77
freshpotato99 <18679917958@163.com>
2025-08-08 14:20:46 +0800

    add tf apply

--- main.tf
+++ main.tf
@@ -2,8 +2,13 @@ provider "alicloud" {
   region = "cn-hangzhou"
 }
 
-# 这里可以添加你的数据源(data)查询
-# 例如：
-# data "alicloud_regions" "current" {
-#   current = true
-# }
+# 创建最简单的OSS Bucket
+resource "alicloud_oss_bucket" "simple_bucket" {
+  bucket = "my-simple-tf-bucket"  # 改成全球唯一的名称
+  acl    = "private"              # 基础访问权限
+}
+
+# 输出Bucket名称（可选）
+output "bucket_name" {
+  value = alicloud_oss_bucket.simple_bucket.bucket
+}
